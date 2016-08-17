578a03812d9de84e6f6c3ab2c6ee2d643a264ee2
Colin <colin.diesh@gmail.com>
2016-08-17 11:07:06 -0500

    Add test data

--- js/View/FeatureGlyph/AlignmentBase.js
+++ js/View/FeatureGlyph/AlignmentBase.js
@@ -24,74 +24,7 @@ function(
 
         _defaultConfig: function() {
             return this._mergeConfigs(dojo.clone(this.inherited(arguments)), {
-                style: {
-                    color: function(feature, path, glyph, track) {
-                        var strand = feature.get('strand');
-                        if (Math.abs(strand) != 1 && strand != '+' && strand != '-')
-                            return track.colorForBase('reference');
-                        else if (track.config.useXS) {
-                            var xs = feature._get('xs');
-                            var strand = {'-': 'color_rev_strand', '+': 'color_fwd_strand'}[xs];
-                            if (!strand) strand = 'color_nostrand';
-                            return glyph.getStyle(feature, strand);
-                        }
-                        else if (feature.get('multi_segment_template')) {
-                            var revflag = feature.get('multi_segment_first');
-                            if (feature.get('multi_segment_all_correctly_aligned')) {
-                                if (revflag || !track.config.useReverseTemplate) {
-                                    return strand == 1 || strand == '+'
-                                    ? glyph.getStyle(feature, 'color_fwd_strand')
-                                    : glyph.getStyle(feature, 'color_rev_strand');
-                                } else {
-                                    return strand == 1 || strand == '+'
-                                    ? glyph.getStyle(feature, 'color_rev_strand')
-                                    : glyph.getStyle(feature, 'color_fwd_strand');
-                                }
-                            }
-                            if (feature.get('multi_segment_next_segment_unmapped')) {
-                                if (revflag || !track.config.useReverseTemplate) {
-                                    return strand == 1 || strand == '+'
-                                    ? glyph.getStyle(feature, 'color_fwd_missing_mate')
-                                    : glyph.getStyle(feature, 'color_rev_missing_mate');
-                                } else {
-                                    return strand == 1 || strand == '+'
-                                    ? glyph.getStyle(feature, 'color_rev_missing_mate')
-                                    : glyph.getStyle(feature, 'color_fwd_missing_mate');
-                                }
-                            }
-                            if (feature.get('seq_id') == feature.get('next_seq_id')) {
-                                if (revflag || !track.config.useReverseTemplate) {
-                                    return strand == 1 || strand == '+'
-                                    ? glyph.getStyle(feature, 'color_fwd_strand_not_proper')
-                                    : glyph.getStyle(feature, 'color_rev_strand_not_proper');
-                                } else {
-                                    return strand == 1 || strand == '+'
-                                    ? glyph.getStyle(feature, 'color_rev_strand_not_proper')
-                                    : glyph.getStyle(feature, 'color_fwd_strand_not_proper');
-                                }
-                            }
-                          // should only leave aberrant chr
-                            return strand == 1 || strand == '+'
-                                  ? glyph.getStyle(feature, 'color_fwd_diff_chr')
-                                  : glyph.getStyle(feature, 'color_rev_diff_chr');
-                        }
-                        return strand == 1 || strand == '+'
-                             ? glyph.getStyle(feature, 'color_fwd_strand')
-                             : glyph.getStyle(feature, 'color_rev_strand');
-                    },
-                    color_fwd_strand_not_proper: '#ECC8C8',
-                    color_rev_strand_not_proper: '#BEBED8',
-                    color_fwd_strand: '#EC8B8B',
-                    color_rev_strand: '#8F8FD8',
-                    color_fwd_missing_mate: '#D11919',
-                    color_rev_missing_mate: '#1919D1',
-                    color_fwd_diff_chr: '#000000',
-                    color_rev_diff_chr: '#969696',
-                    color_nostrand: '#999999',
-                    border_color: null,
-
                     strandArrow: false,
-
                     height: 7,
                     marginBottom: 1,
                     showMismatches: true,
--- js/main.js
+++ js/main.js
@@ -2,18 +2,16 @@ define([
     'dojo/_base/declare',
     'JBrowse/Plugin'
 ],
-       function(
-           declare,
-           JBrowsePlugin
-       ) {
-           return declare(JBrowsePlugin,
-               {
-                   constructor: function(args) {
-                       var browser = args.browser;
+function(
+    declare,
+    JBrowsePlugin
+) {
+    return declare(JBrowsePlugin, {
+        constructor: function(args) {
+            var browser = args.browser;
 
-        // do anything you need to initialize your plugin here
-                       console.log('QualityGlyphs plugin starting');
+            console.log('QualityGlyphs plugin starting');
 
-                   }
-               });
-       });
+        }
+    });
+});
